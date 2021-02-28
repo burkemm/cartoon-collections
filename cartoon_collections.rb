5412607ed08ef64d9abb6912e308a186b6b985f3
burkemm <burkemm84@gmail.com>
2021-02-27 21:41:12 -0600

    commit tree 340b30cbfcc9fa15c29e3386fa39a02de2435813

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,21 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarfs, number)# code an argument here
+  dwarfs = ["Dopey", "Grumpy", "Bashful",]
+  puts " #{number} #{dwarfs}"
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+
+def summon_captain_planet(planeteer_call)# code an argument here
+  planeteer_call = ["earth", "wind", "fire", "water", "heart"]
+  count = 0
+  while count < planeteer_call.length
+    #word = words[count]
+    planeteer_call[0] = planeteer_call[0].upcase
+    count += 1
+  end
+
+  return planeteer_call.join(" ").upcase
+  return 
   # Your code here
 end
 
